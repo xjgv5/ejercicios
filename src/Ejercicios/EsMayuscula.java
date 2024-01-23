2188fa95d967af4f1776f7e39f7cb9a85c83a23c
Javier Gomez <javier.gov@outlook.es>
2024-01-22 22:04:44 -0600

    Saber si una cadena es minuscula o mayuscula (Optimizacion)

--- src/Ejercicios/EsMayuscula.java
+++ src/Ejercicios/EsMayuscula.java
@@ -7,14 +7,13 @@ public class EsMayuscula {
         //programa que especifica si un caracter o cadena es minuscula o mayuscula
         String cadena;
         Scanner sc = new Scanner(System.in);
-        boolean esMayuscula;
         String resultado;
 
         System.out.println("Ingresa una cadena para saber si esta en mayuscula o minuscula ");
         cadena = sc.nextLine();
 
-        esMayuscula = cadena.equals(cadena.toUpperCase()) ? true : false;
-        resultado = esMayuscula == true ? "mayuscula" : "minuscula";
+        resultado = cadena.equals(cadena.toUpperCase()) == true ? "mayuscula" : "minuscula";
+        
         System.out.println("La cadena capturada esta en " + resultado);
     }
 }
